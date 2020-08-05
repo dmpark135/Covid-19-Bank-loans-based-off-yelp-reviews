bf7239a8d0980592453aecf7ec5e66fa288468f0
dave <dmpark135@gmail.com>
2020-08-05 19:29:22 -0400

    Sample file for json
    sample file

---
+++ BusinessLoans.py
@@ -0,0 +1,24 @@
+!git add "file.py"
+!git commit -m "My commit"
+!git push origin master
+
+import json
+
+with open('C:\\Users\\agoge\\Desktop\\yelp_data\\yelp_academic_dataset_business.json') as f:
+ 
+    f = open("C:\\Users\\agoge\\Desktop\\yelp_data\\yelp_academic_dataset_business.json", "r")
+
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+print(f.readline())
+
+ # data = json.load(f)
+#print(data)
+ #C:\\Users\\agoge\\Desktop\\yelp_data\\yelp_academic_dataset_review.json
